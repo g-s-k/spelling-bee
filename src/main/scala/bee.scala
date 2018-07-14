d088f2494cbfa6a4efbe9b875f5368578cf667f5
George Kaplan <george@georgekaplan.xyz>
2018-07-13 23:42:35 -0400

    picking the letters

--- src/main/scala/bee.scala
+++ src/main/scala/bee.scala
@@ -1,5 +1,17 @@
+import scala.util.Random
+
 object Main {
-    def main(args: Array[String]) {
-      println("hello george")
-    }
+  def main(args: Array[String]) {
+    // all of the letters
+    val allLetters = ('A'.to('Z')) toList
+
+    // select seven at random
+    val random = new Random
+    val pickOne = (l: List[Char]) => l(random.nextInt(l.length))
+    val centerLetter = pickOne(allLetters)
+    val ringLetters = for (index <- 1 to 6) yield pickOne(allLetters)
+
+    println(centerLetter)
+    println(ringLetters)
+  }
 }
