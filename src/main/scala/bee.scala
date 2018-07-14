9f5f2a793e2322b9c0259e06884843a45a4cb3c3
George Kaplan <george@georgekaplan.xyz>
2018-07-14 01:16:34 -0400

    adapted to work with source words

--- src/main/scala/bee.scala
+++ src/main/scala/bee.scala
@@ -3,14 +3,14 @@ import scala.util.Random
 object Main {
   def main(args: Array[String]) {
     // all of the letters
-    val allLetters = ('A'.to('Z')) toList
+    val allLetters = ('a' to 'z') toList
 
     // select seven at random
     val random = new Random
     // TODO: better heuristic than purely random (need vowels etc.)
     val pickOne = (l: List[Char]) => l(random.nextInt(l.length))
     val centerLetter = pickOne(allLetters)
-    val ringLetters = for (_ <- 1 to 6) yield pickOne(allLetters)
+    val ringLetters = (1 to 6).map((_: Int) => pickOne(allLetters)).toSet
 
     // TODO: load dictionary and find words spelled with only these letters
 
