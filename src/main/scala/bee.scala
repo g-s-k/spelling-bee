ef08806edcc248139bbd5417d3f2c9b204cb731c
George Kaplan <george@georgekaplan.xyz>
2018-07-14 01:17:42 -0400

    loading and filtering words

--- src/main/scala/bee.scala
+++ src/main/scala/bee.scala
@@ -1,4 +1,5 @@
 import scala.util.Random
+import scala.io.Source
 
 object Main {
   def main(args: Array[String]) {
@@ -12,7 +13,12 @@ object Main {
     val centerLetter = pickOne(allLetters)
     val ringLetters = (1 to 6).map((_: Int) => pickOne(allLetters)).toSet
 
-    // TODO: load dictionary and find words spelled with only these letters
+    // load dictionary and find words spelled with only these letters
+    val words = Source.fromResource("english-words/words_alpha.txt")
+      .getLines()
+      .filter((w: String) => w.length() > 3)
+      .filter((w: String) => w contains centerLetter)
+      .filter((w: String) => w.toSet.diff(ringLetters + centerLetter).size == 0)
 
     // TODO: define template for prompt to maximize information conveyed to user
 
