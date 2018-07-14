4fbdac8473cd6d9d4dbd835434eefd9b6a1d8296
George Kaplan <george@georgekaplan.xyz>
2018-07-14 00:22:13 -0400

    added todos

--- src/main/scala/bee.scala
+++ src/main/scala/bee.scala
@@ -7,9 +7,18 @@ object Main {
 
     // select seven at random
     val random = new Random
+    // TODO: better heuristic than purely random (need vowels etc.)
     val pickOne = (l: List[Char]) => l(random.nextInt(l.length))
     val centerLetter = pickOne(allLetters)
     val ringLetters = for (_ <- 1 to 6) yield pickOne(allLetters)
+
+    // TODO: load dictionary and find words spelled with only these letters
+
+    // TODO: define template for prompt to maximize information conveyed to user
+
+    // TODO: prompt user for words until all are found or they give up
+
+    // TODO: display score
 
     println(centerLetter)
     println(ringLetters)
